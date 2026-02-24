4708d5a5fd9c81a222d0a8ce5d9f70b1e624b85b
hARSh2018-arch <harshsinghtomar95@gmail.com>
2026-02-24 10:29:59 +0530

    Add UC5_ArrayInit class to initialize and print array

---
+++ UC5_ArrayInit.java
@@ -0,0 +1,9 @@
+public class UC5_ArrayInit {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        for (int num : numbers) {
+            System.out.println("Number: " + num);
+        }
+    }
+}
