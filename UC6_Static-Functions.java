adb69f7c486861f95d4512056c4c25958ddb8b88
hARSh2018-arch <harshsinghtomar95@gmail.com>
2026-02-24 10:31:56 +0530

    Add static function to greet users

---
+++ UC6_Static-Functions.java
@@ -0,0 +1,11 @@
+public class UC6_StaticFunctions {
+    // A static function that can be called without creating an object
+    public static void greet() {
+        System.out.println("Welcome to OOPS Banner App!");
+    }
+
+    public static void main(String[] args) {
+        // Call the static function directly
+        greet();
+    }
+}
