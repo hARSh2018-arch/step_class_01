61755f85575c996d59d25137f4c60742db736fb7
hARSh2018-arch <harshsinghtomar95@gmail.com>
2026-02-24 10:27:14 +0530

    Add UC4_ArrayLoops class to print features

---
+++ UC4_ArrayLoops.java
@@ -0,0 +1,9 @@
+public class UC4_ArrayLoops {
+    public static void main(String[] args) {
+        String[] features = {"Print OOPS", "Banner", "String Join", "Array Loops"};
+        
+        for (String feature : features) {
+            System.out.println("Feature: " + feature);
+        }
+    }
+}
